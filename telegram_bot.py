e329c87f9d22bdffc50c6f380be7de52d6eab48a
Your Name <you@example.com>
2022-01-17 16:54:45 +0300

    provide weather provider code

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,7 +11,7 @@ bot = telebot.TeleBot("5017425940:AAEW7c0gjuZFfDXYcYxRDT-90qvk9CSMYlU")
 def get_weather():
 	url = "https://community-open-weather-map.p.rapidapi.com/weather"
 
-	querystring = {"lat":"0","lon":"0","lang":"ua"}
+	querystring = {"lat":"0","lon":"0","lang":"ua", "units": "metric"}
 
 	headers = {
 	    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
@@ -23,9 +23,20 @@ def get_weather():
 	return response.text
 
 
+def format_weather(weather):
+	return f'''
+	{weather["weather"][0]["description"]}
+
+	Температура: {weather["weather"][0]["main"]["temp"]}
+	Скорость ветра: {weather["weather"][0]["main"]["wind"]["speed"]}
+
+	'''
+
+
 @bot.message_handler(commands=['weather'])
 def send_weather(message):
-	bot.reply_to(message, get_weather())
+	raw_weather = get_weather()
+	bot.reply_to(message, format_weather(raw_weather))
 
 
 @bot.message_handler(commands=['start', 'help'])
