9615e52d7f3831740b041460d3e3f5a64f566460
Bogdan Danilyuk <bogdan.danilyuk@eosda.com>
2022-01-20 17:53:12 +0200

    step 3

--- telegram_bot.py
+++ telegram_bot.py
@@ -52,3 +52,20 @@ from telebot import types
 
 bot = telebot.TeleBot('5187725132:AAFHfAToPnKkCyeo0G43FuiZajpkMVZFxhM')
 WEATHER_API_KEY = '5b642e1561mshe54d684b73ad18bp1121c4jsn79cf637b53a1'
+
+
+@bot.message_handler(commands=['start', 'help'])
+def handle_start(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_weather = types.KeyboardButton(text='Узнать погоду', request_location=True)
+    keyboard.add(button_weather)
+
+    command_weather = types.BotCommand('weather', 'Get weather for Sievierodonetsk')
+    bot.set_my_commands(commands=[command_weather])
+
+    bot.send_message(message.chat.id, 'Привет! Нажми на кнопку или , чтобы я мог узнать погоду для тебя.',
+                     reply_markup=keyboard)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
