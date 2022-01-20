33a537e3ffcc465b3529d43853919b353f2397dc
Bogdan Danilyuk <bogdan.danilyuk@eosda.com>
2022-01-20 17:51:16 +0200

    step 2

--- telegram_bot.py
+++ telegram_bot.py
@@ -46,3 +46,9 @@
 WEATHER_API_KEY = '5b642e1561mshe54d684b73ad18bp1121c4jsn79cf637b53a1'
 '''
 
+import requests
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5187725132:AAFHfAToPnKkCyeo0G43FuiZajpkMVZFxhM')
+WEATHER_API_KEY = '5b642e1561mshe54d684b73ad18bp1121c4jsn79cf637b53a1'
