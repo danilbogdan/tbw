2842ea8589a72e82761644c966b6fe13ffe67b14
Your Name <you@example.com>
2022-01-22 13:31:37 +0300

    remove token

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,7 +3,7 @@ import telebot
 from telebot import types
 
 WEATHER_API_KEY = '5b642e1561mshe54d684b73ad18bp1121c4jsn79cf637b53a1'
-TELEGRAM_BOT_TOKEN = '5183862802:AAGKOmPEalqEuBUt7ze9SYzJvRUPo-153AM'
+TELEGRAM_BOT_TOKEN = ''
 
 
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
