e88daf12c805c854c3743e6772c116409075faf3
Your Name <you@example.com>
2022-01-22 13:05:56 +0300

    refactor

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,51 +1,3 @@
-'''
-{
-   "coord":{
-      "lon":-0.1257,
-      "lat":51.5085
-   },
-   "weather":[
-      {
-         "id":804,
-         "main":"Clouds",
-         "description":"overcast clouds",
-         "icon":"04d"
-      }
-   ],
-   "base":"stations",
-   "main":{
-      "temp":40.87,
-      "feelslike":35.06,
-      "tempmin":38.7,
-      "temp_max":42.98,
-      "pressure":1037,
-      "humidity":64
-   },
-   "visibility":10000,
-   "wind":{
-      "speed":9.22,
-      "deg":330
-   },
-   "clouds":{
-      "all":90
-   },
-   "dt":1642691408,
-   "sys":{
-      "type":2,
-      "id":2019646,
-      "country":"GB",
-      "sunrise":1642665284,
-      "sunset":1642696071
-   },
-   "timezone":0,
-   "id":2643743,
-   "name":"London",
-   "cod":200
-}
-
-WEATHER_API_KEY = '5b642e1561mshe54d684b73ad18bp1121c4jsn79cf637b53a1'
-'''
-
 import requests
 import telebot
 from telebot import types
@@ -70,11 +22,11 @@ def get_current_weather(location):
 
 def format_weather(weather):
     return f'''
-	{weather['weather'][0]['description']}
+    {weather['weather'][0]['description']}
 
-	Температура: {weather['main']['temp']}°
-	Швидкість вітру: {weather['wind']['speed']}
-	'''
+    Температура: {weather['main']['temp']}°
+    Швидкість вітру: {weather['wind']['speed']}
+    '''
 
 
 def get_icon_url(weather):
@@ -95,4 +47,49 @@ def handle_start(message):
 
 
 if __name__ == '__main__':
+    '''
+    {
+       "coord":{
+          "lon":-0.1257,
+          "lat":51.5085
+       },
+       "weather":[
+          {
+             "id":804,
+             "main":"Clouds",
+             "description":"overcast clouds",
+             "icon":"04d"
+          }
+       ],
+       "base":"stations",
+       "main":{
+          "temp":40.87,
+          "feelslike":35.06,
+          "tempmin":38.7,
+          "temp_max":42.98,
+          "pressure":1037,
+          "humidity":64
+       },
+       "visibility":10000,
+       "wind":{
+          "speed":9.22,
+          "deg":330
+       },
+       "clouds":{
+          "all":90
+       },
+       "dt":1642691408,
+       "sys":{
+          "type":2,
+          "id":2019646,
+          "country":"GB",
+          "sunrise":1642665284,
+          "sunset":1642696071
+       },
+       "timezone":0,
+       "id":2643743,
+       "name":"London",
+       "cod":200
+    }
+    '''
     bot.infinity_polling()
