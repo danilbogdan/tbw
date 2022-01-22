fb352771fa57082a9889dc02b2b126696014fe11
Your Name <you@example.com>
2022-01-22 12:43:26 +0300

    remove garbage code

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,54 +1,6 @@
-'''
-{
-   "coord":{
-      "lon":-0.1257,
-      "lat":51.5085
-   },
-   "weather":[
-      {
-         "id":804,
-         "main":"Clouds",
-         "description":"overcast clouds",
-         "icon":"04d"
-      }
-   ],
-   "base":"stations",
-   "main":{
-      "temp":40.87,
-      "feelslike":35.06,
-      "tempmin":38.7,
-      "temp_max":42.98,
-      "pressure":1037,
-      "humidity":64
-   },
-   "visibility":10000,
-   "wind":{
-      "speed":9.22,
-      "deg":330
-   },
-   "clouds":{
-      "all":90
-   },
-   "dt":1642691408,
-   "sys":{
-      "type":2,
-      "id":2019646,
-      "country":"GB",
-      "sunrise":1642665284,
-      "sunset":1642696071
-   },
-   "timezone":0,
-   "id":2643743,
-   "name":"London",
-   "cod":200
-}
-
-WEATHER_API_KEY = '5b642e1561mshe54d684b73ad18bp1121c4jsn79cf637b53a1'
-'''
-
 import requests
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('5187725132:AAFHfAToPnKkCyeo0G43FuiZajpkMVZFxhM')
+bot = telebot.TeleBot()
 WEATHER_API_KEY = '5b642e1561mshe54d684b73ad18bp1121c4jsn79cf637b53a1'
