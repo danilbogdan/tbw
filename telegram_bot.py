9e466f414e116e0697821a0e57393818c9bd7866
Your Name <you@example.com>
2022-01-22 12:45:15 +0300

    refactor

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,5 +2,8 @@ import requests
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot()
 WEATHER_API_KEY = '5b642e1561mshe54d684b73ad18bp1121c4jsn79cf637b53a1'
+TELEGRAM_BOT_TOKEN = ''
+
+
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
